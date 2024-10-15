601263112b2b946983a82538c1f0cfb21f058f10
Yaser.ta <yasertarrab10@gmail.com>
2024-10-15 20:49:54 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,9 @@
+// this is class for haeder "active"
+let header = document.querySelector("header")
+window.onscroll = function(){
+    if(this.scrollY >=50){
+        header.classList.add("active")
+    }else{
+        header.classList.remove("active")
+    }
+}
